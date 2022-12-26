1dad5110849098302e3bbae86885b69465065eb7
joseph-468 <josephalba468@gmail.com>
2022-12-26 23:29:14 +0000

    Added collisions, display score and beginning of health system

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 # CONSTANTS
@@ -16,8 +18,23 @@ def main():
     clock = pygame.time.Clock()
 
     # Variables
+    font = pygame.font.SysFont("", 48)
+    bullet_speed = 4
+    spawn_locations = [(0, 395), (395, 0), (395, 792), (792, 395)]
+    counter = 0
+    score = 0
+    health = 5
     player = pygame.Rect(383, 383, 32, 8)
+    hit_box = pygame.Rect(383, 383, 32, 32)
+    bullets = []
 
+    # Classes
+    class Bullet:
+        def __init__(self, rectangle, direction):
+            self.rectangle = rectangle
+            self.direction = direction
+
+    # Functions
     def move(key):
         if key == pygame.K_w:
             player.size = (32, 8)
@@ -36,8 +53,52 @@ def main():
             player.x = 407
             player.y = 383
 
+    def move_bullets():
+        for bullet in bullets:
+            if bullet.direction == 0:
+                bullet.rectangle.x += bullet_speed
+            elif bullet.direction == 1:
+                bullet.rectangle.y += bullet_speed
+            elif bullet.direction == 2:
+                bullet.rectangle.y -= bullet_speed
+            elif bullet.direction == 3:
+                bullet.rectangle.x -= bullet_speed
+
+    def spawn_bullets():
+        position = random.choice(spawn_locations)
+        spawn_side = spawn_locations.index(position)
+        bullets.append(Bullet(rectangle=pygame.Rect(position[0], position[1], 8, 8), direction=spawn_side))
+
+    def check_collisions(game_score, game_health):
+        for x, bullet in enumerate(bullets):
+            if player.colliderect(bullet.rectangle):
+                del bullets[x]
+                game_score += 1
+            elif hit_box.colliderect(bullet.rectangle):
+                del bullets[x]
+                game_health -= 1
+        return game_score, game_health
+
+    def render_game():
+        screen.fill(WHITE)
+        # Text
+        score_text = font.render(f"Score: {score}", True, (0, 0, 0))
+        health_text = font.render(f"Lives: {health}", True, (0, 0, 0))
+        screen.blit(score_text, (20, 20))
+        screen.blit(health_text, (20, 68))
+        # Objects
+        pygame.draw.rect(screen, (0, 0, 0), hit_box, 2)
+        pygame.draw.rect(screen, (255, 0, 0), player)
+        for bullet in bullets:
+            pygame.draw.rect(screen, (0, 0, 255), bullet.rectangle)
+        pygame.display.update()
+
     # Game loop
     while True:
+        if counter == 30:
+            counter = 0
+        else:
+            counter += 1
         # Gets input
         for event in pygame.event.get():
             # Handle quitting
@@ -46,15 +107,17 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
-                # Handle useful input
+                # Handle input
                 move(event.key)
 
-        # Do stuff
+        # Calculate things
+        move_bullets()
+        if counter == 30:
+            spawn_bullets()
+        score, health = check_collisions(score, health)
 
-        # Updates screen
-        screen.fill(WHITE)
-        pygame.draw.rect(screen, (255, 0, 0), player)
-        pygame.display.update()
+        # Update screen
+        render_game()
         clock.tick(FPS)
 
 
